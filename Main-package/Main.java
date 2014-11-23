99ba989330dcbd5f75ea49991f35d2a27059bd8e
nomelif <theo.friberg@gmail.com>
2014-11-23 20:33:34 +0200

    Made main's skeleton meaningful

--- Main-package/Main.java
+++ Main-package/Main.java
@@ -1,12 +1,31 @@
 /*
 
-By Théo Friberg under GNU GPL v. 3
+ By Théo Friberg under GNU GPL v. 3
 
 
-Using MagicMap:
+ Using MagicMap:
 
-MagicMap is invoked by typing java MagiMap.class -cli [path to input file] [path to output file]
-*/
+ MagicMap is invoked by typing java MagiMap.class -cli [path to input file] [path to output file]
+ */
+
+/***********************************************
+ *                  IMPORTS                    *
+ ***********************************************/
+
+// Image-related
+
+import java.awt.image.BufferedImage;
+
+// IO-related
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/***********************************************
+ * Main-class *
+ ***********************************************/
 
 public class Main {
 
@@ -14,47 +33,95 @@ public class Main {
 	 * @param args
 	 * 
 	 */
+
+	// Main-function
+
 	public static void main(String[] args) {
-		
+
 		// See if invoked with -cli -argument
-		
-		if(arg.length > 0){
-			
-			// App is invoked with -cli -argument
-			
-			if(args[0] == "-cli"){
-				
-				// Does the app get the right arguments?
-				
+
+		if (args.length > 0) {
+
+			// Application is invoked with -cli -argument
+
+			if (args[0] == "-cli") {
+
+				// Does the application get the right arguments?
+
 				boolean right_args = false;
-				
-				if(args.length > 2){
-					
-					/*
-					
-					Processing of the image will be here and will be implemented shortly
-					
-					*/
-					
-					
-				}else{
-					
-					// Too few arguments, app was invoked incorrectly
-					
+
+				if (args.length > 2) {
+
+					// If the output-image's name is of type name.extension
+
+					if (args[2].split(System.getProperty("file.separator2"))[args[2]
+							.split(System.getProperty("file.separator2")).length - 1]
+							.split("\\.").length > 1) {
+
+						// Get the extension of the output-file
+
+						String format = args[2].split(System
+								.getProperty("file.separator2"))[args[2]
+								.split(System.getProperty("file.separator2")).length - 1]
+								.split("\\.")[args[2].split(System
+								.getProperty("file.separator2"))[args[2]
+								.split(System.getProperty("file.separator2")).length - 1]
+								.split("\\.").length - 1];
+
+						// Try opening the input-image
+
+						BufferedImage img = null;
+
+						try {
+
+							// Open input-image
+
+							img = ImageIO.read(new File(args[1]));
+
+							/*
+							 * Image processing here
+							 */
+
+							try {
+								File outputfile = new File(args[2]);
+								ImageIO.write(img, format, outputfile);
+							} catch (IOException e) {
+								System.err.println("Can't save image to '"
+										+ args[0] + "'.");
+							}
+
+						} catch (IOException e) {
+
+							// Something went wrong:
+
+							System.err.println("Can't open image '" + args[0]
+									+ "'.");
+
+							// Tell that something went wrong
+
+							right_args = false;
+
+						}
+					}
+
+				} else {
+
+					// Too few arguments, application was invoked incorrectly
+
 					right_args = false;
 				}
-				
-				// If the app wasn't invoked correctly
-				
-				if(right_args == false){
-					
-					System.out.println("Usage: java MagicMap.class -cli [input file] [output file]");
+
+				// If the application wasn't invoked correctly
+
+				if (right_args == false) {
+
+					System.out
+							.println("Usage: java MagicMap.class -cli [input file] [output file]");
 				}
-				
+
 			}
 		}
-	
-	
+
 	}
 
 }
